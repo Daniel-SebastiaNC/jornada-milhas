eb5214f4b99b02348b8c9b5de5b674f54a63d3fb
Daniel Sebastian Nunes Cavalcanti <daniel.sebastian.nc@gmail.com>
2024-10-02 21:09:29 -0300

    modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { ModalComponent } from './shared/modal/modal.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
+
 
 @NgModule({
   declarations: [
@@ -35,7 +39,9 @@ import { MatNativeDateModule } from '@angular/material/core';
     HomeComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
-    FormBuscaComponent
+    FormBuscaComponent,
+    ModalComponent,
+    BotaoControleComponent
   ],
   imports: [
     BrowserModule,
@@ -49,7 +55,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatDialogModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-form-busca',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './form-busca.component.scss',
 })
 export class FormBuscaComponent {
-  
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    })
+  }
 }
